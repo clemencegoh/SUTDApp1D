fcab5254111e678ab147384cb02a351c5b2c0843
Clemence Goh <clemencegoh@users.noreply.github.com>
2017-10-27 20:09:09 +0800

    myPortal

---
+++ JavaLogics/myPortal/myPortalTest.java
@@ -0,0 +1,15 @@
+package AppLogic.myPortal;
+
+import java.util.Arrays;
+
+/**
+ * Created by Clemence on 10/20/2017.
+ */
+
+public class myPortalTest {
+    public static void main(String[] args) {
+        myPortal Profile = new myPortal();
+        /* getTimeTableDetails(username,password) */
+        String[][] s = Profile.getTimeTableDetails();
+    }
+}
