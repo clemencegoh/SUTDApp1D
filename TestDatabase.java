ceb64b60782a373d366186471443b0163fa690db
tantingyu <32908553+tantingyu@users.noreply.github.com>
2017-10-30 09:42:09 +0800

    Add files via upload

---
+++ TestDatabase.java
@@ -0,0 +1,9 @@
+package com.example.tingyu.scheduler;
+
+public class TestDatabase {
+
+    public static void main (String[] args) {
+        Event e = new Event("", "Sun, 29 Oct 2017", "19:30", "21:30", "MPH",
+                "1002169", "tl;dr");
+    }
+}
