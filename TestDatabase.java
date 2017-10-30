a63473456751eb563ff7373ef089852bc480d2f5
tantingyu <32908553+tantingyu@users.noreply.github.com>
2017-10-30 18:14:55 +0800

    Delete TestDatabase.java

--- TestDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.tingyu.scheduler;
-
-public class TestDatabase {
-
-    public static void main (String[] args) {
-        Event e = new Event("", "Sun, 29 Oct 2017", "19:30", "21:30", "MPH",
-                "1002169", "tl;dr");
-    }
-}
