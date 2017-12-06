21e6486969b4aea3e655b355f8b09fdfd6e3963e
swonlek <chonglokswen97@hotmail.com>
2017-12-07 00:32:40 +0800

    UI: converted Event Manager (dont touch Event Manager yet) and Calendar to fragment, left subscriptionsandevents, profile page, settings for each fragments. Can start integrating Login Activity and Calendar.

--- SUTDApp1D/app/src/main/java/com/ft4sua/sutdapp1d/MainActivity.java
+++ SUTDApp1D/app/src/main/java/com/ft4sua/sutdapp1d/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -18,49 +17,52 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.ft4sua.sutdapp1d.EventPackage.AddEventActivity;
-import com.ft4sua.sutdapp1d.EventPackage.EventManager;
-
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawer;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
-    private Intent addEventIntent;
     private Intent profilePageIntent;
     private Intent subsEventsIntent;
     private Intent eventManagerIntent;
+    private Intent calendarIntent;
+    private Intent addEventIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        // set initial fragment
+        CalendarFragment calFrag = new CalendarFragment();
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, calFrag);
+        fragmentTransaction.commit();
+
+        // Toolbar to replace ActionBar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_add);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                addEventIntent = new Intent(MainActivity.this, AddEventActivity.class);
-                startActivity(addEventIntent);
-            }
-        });
-
-        // TODO: either convert SubEventactivity to fragment (might screw up the tab fragments)
-        // TODO: when clicking calendar , it's actually back press. idk
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // Drawer view
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        // nav bar
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        navigationView.setCheckedItem(R.id.nav_main);
 
+        // nav header
         View headerView = navigationView.inflateHeaderView(R.layout.nav_header_main);
         TextView studentName = (TextView) headerView.findViewById(R.id.student_name);
         ImageView profilePic = (ImageView) headerView.findViewById(R.id.profile_pic);
+        //TODO: if database records student names, change this to display student name. Otherwise just student ID. Link to settings?
+        // or edit student name from here idk
+        studentName.setText(R.string.student_name);
         profilePic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,17 +70,12 @@ public class MainActivity extends AppCompatActivity
                 startActivity(profilePageIntent);
             }
         });
-        //TODO: if database records student names, change this to display student name. Otherwise just student ID. Link to settings?
-        // or edit student name from here idk
-        studentName.setText(R.string.student_name);
-
 
         /***---Database Test
          * Check out this class for sample usage---***/
         //DatabaseTester test=new DatabaseTester();
         //test.test(this);
     }
-
 
     @Override
     public void onBackPressed() {
@@ -103,9 +100,8 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            // return Settings page for Calendar.
             return true;
         }
 
@@ -116,30 +112,47 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        // note! must do the fragment thing inside the if clause
         int id = item.getItemId();
-
         if (id == R.id.nav_event_manager) {
             Toast.makeText(MainActivity.this, "Events u created", Toast.LENGTH_SHORT).show();
-            eventManagerIntent = new Intent(MainActivity.this, EventManager.class);
-            startActivity(eventManagerIntent);
-
-
+            Class fragmentClass = EventManagerFragment.class;
+            android.support.v4.app.Fragment fragment = null;
+            try {
+                fragment = (android.support.v4.app.Fragment) fragmentClass.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
         } else if (id == R.id.nav_subs_events) {
             Toast.makeText(MainActivity.this, "Events u subbed", Toast.LENGTH_SHORT).show();
             subsEventsIntent = new Intent(MainActivity.this, SubsEventsActivity.class);
             startActivity(subsEventsIntent);
 
         } else if (id == R.id.nav_sync_timetable) {
-
+            Toast.makeText(MainActivity.this, "Timetable synced", Toast.LENGTH_SHORT).show();
         } else if (id==R.id.action_logout) {
             SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
             prefs.edit().putInt(getString(R.string.login_key), 0).apply();
             startActivity(new Intent(this, LoginActivity.class));
             finish();
-        } else if (id==R.id.nav_main){
+        } else if (id==R.id.nav_cal){
+//            calendarIntent = new Intent(MainActivity.this, CalendarActivity.class);
+//            startActivity(calendarIntent);
+            // set initial fragment
+            Class fragmentClass = CalendarFragment.class;
+            android.support.v4.app.Fragment fragment = null;
+            try {
+                fragment = (android.support.v4.app.Fragment) fragmentClass.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
         }
+;
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
