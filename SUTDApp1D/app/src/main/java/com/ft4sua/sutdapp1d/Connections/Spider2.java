4190c7790b3254a8c18167fb50505ae236121ae4
TriEightz <yujin_g1995@hotmail.com>
2017-12-09 14:58:39 +0800

    Edited program to fix dates

--- SUTDApp1D/app/src/main/java/com/ft4sua/sutdapp1d/Connections/Spider2.java
+++ SUTDApp1D/app/src/main/java/com/ft4sua/sutdapp1d/Connections/Spider2.java
@@ -1,10 +1,9 @@
-package com.ft4sua.sutdapp1d.Connections;
+package SpiderSingle;
 
 /**
  * Created by Yu Jin on 10/20/2017.
  */
 
-import com.ft4sua.sutdapp1d.DatabasePackage.Event;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,12 +11,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-
+import java.util.*;
 
 public class Spider2 {
     private static final String USER_AGENT =
@@ -68,18 +62,20 @@ public class Spider2 {
      *            - The word or string to look for
      * @return whether or not the word was found
      */
-    public ArrayList<Event> search()
+    public ArrayList<String[]> search()
     {
         // Defensive coding. This method should only be used after a successful crawl.
         if (crawled == false){
             System.out.println("Crawl(link) first");
-            return new ArrayList<Event>();
+            //return new ArrayList<Event>();
+            return new ArrayList<String[]>();
         }
 
         if(this.htmlDocument == null)
         {
             System.out.println("ERROR! Call crawl() before performing analysis on the document");
-            return new ArrayList<Event>();
+            //return new ArrayList<Event>();
+            return new ArrayList<String[]>();
         }
         System.out.println("Searching for the word " + "...");
 
@@ -89,7 +85,13 @@ public class Spider2 {
         Set<Character> numbers = new HashSet<Character>();
         numbers.add('0');numbers.add('1');numbers.add('2');numbers.add('3');numbers.add('4');numbers.add('5');numbers.add('6');
         numbers.add('7');numbers.add('8');numbers.add('9');
-        ArrayList<Event> week_events = new ArrayList<Event>();
+       // ArrayList<Event> week_events = new ArrayList<Event>();
+        ArrayList<String[]> week_events = new ArrayList<>();
+        String[] monthlist = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+        HashMap<String,Integer> monthconvert = new HashMap<>();
+        monthconvert.put("Jan",0); monthconvert.put("Feb",1); monthconvert.put("Mar",2); monthconvert.put("Apr", 3);
+        monthconvert.put("May",4); monthconvert.put("Jun",5); monthconvert.put("Jul",6); monthconvert.put("Aug", 7);
+        monthconvert.put("Sep",8); monthconvert.put("Oct",9); monthconvert.put("Nov",10); monthconvert.put("Dec",11);
         int searchpoint;
         String searchlocation;
 
@@ -101,14 +103,73 @@ public class Spider2 {
                 while (rawevent.contains("Read more")) {
                     String[] event = new String[6];
                     String date;
-                    int dateindex= bodyText.indexOf(", 2017");
-                    if (numbers.contains(bodyText.charAt(dateindex-2))){
-                        date = bodyText.substring(dateindex-2,dateindex);
+                    int year = Calendar.getInstance().get(Calendar.YEAR);
+                    System.out.println(Calendar.getInstance().getFirstDayOfWeek());
+                    String yearstring = Integer.toString(year);
+                    int dateindex= bodyText.indexOf(", " + yearstring);
+                    int nextyearindex = bodyText.indexOf(", "+ Integer.toString(year + 1));
+                    int prevyearindex = bodyText.indexOf(", "+ Integer.toString(year - 1));
+                    if (dateindex != -1) {
+                        if (numbers.contains(bodyText.charAt(dateindex - 2))) {
+                            date = bodyText.substring(dateindex - 2, dateindex);
+                        } else {
+                            date = bodyText.substring(dateindex - 1, dateindex);
+                        }
+                    } else if (nextyearindex != -1){
+                        if (numbers.contains(bodyText.charAt(nextyearindex-2))){
+                            date = bodyText.substring(nextyearindex-2,nextyearindex);
+                        } else {
+                            date = bodyText.substring(nextyearindex-1,nextyearindex);
+                        }
                     } else {
-                        date = bodyText.substring(dateindex-1,dateindex);
+                        if (numbers.contains(bodyText.charAt(prevyearindex-2))){
+                            date = bodyText.substring(prevyearindex-2,prevyearindex);
+                        } else {
+                            date = bodyText.substring(prevyearindex-1,prevyearindex);
+                        }
                     }
+
                     dateindex = bodyText.indexOf(" ", 12);
-                    event[0] = daysinweek[i].substring(0, 3) + "," + date + bodyText.substring(dateindex,dateindex+4);;
+                    event[0] = daysinweek[i].substring(0, 3) + "," + date + bodyText.substring(dateindex,dateindex+4);
+                    String month = bodyText.substring(dateindex+1,dateindex+4);
+                    if (month.equals("Jan") || month.equals("Mar") || month.equals("May") || month.equals("Jul")
+                            || month.equals("Aug") || month.equals("Oct") || month.equals("Dec")){
+                        int newdate = Integer.parseInt(date) + i;
+                        if ((newdate/31) > 0){
+                            String newmonth = monthlist[(monthconvert.get(month) + 1)%12];
+                            event[0] = daysinweek[i].substring(0, 3) + "," + Integer.toString(newdate%31) + " " + newmonth;
+                        } else {
+                            event[0] = daysinweek[i].substring(0, 3) + "," + newdate + " " + month;
+                        }
+                    } else if (month.equals("Apr") || month.equals("Jun") ||month.equals("Sep")
+                            || month.equals("Nov") ){
+                        int newdate = Integer.parseInt(date) + i;
+                        if ((newdate/30) > 0){
+                            String newmonth = monthlist[monthconvert.get(month) + 1];
+                            event[0] = daysinweek[i].substring(0, 3) + "," +Integer.toString(newdate%30) + " " + newmonth;
+                        }else {
+                            event[0] = daysinweek[i].substring(0, 3) + "," + newdate + " " + month;
+                        }
+                    } else if (month.equals("Feb")){
+                        int newdate = Integer.parseInt(date) + i;
+                        int leap1 = year %400;
+                        int leap2 = leap1 % 100;
+                        int leap3 = leap2 %4;
+                        int leapyear;
+                        if (leap1 == 0) {
+                            leapyear = 1;
+                        } else if (leap3 == 0 && leap2 != 0){
+                            leapyear = 1;
+                        } else {
+                            leapyear = 0;
+                        }
+                        if ((newdate/(28+leapyear)) > 0){
+                            String newmonth = monthlist[monthconvert.get(month) + 1];
+                            event[0] = daysinweek[i].substring(0, 3) + "," +Integer.toString(newdate%(28+leapyear)) + " " + newmonth;
+                        }else {
+                            event[0] = daysinweek[i].substring(0, 3) + "," + newdate + " " + month;
+                        }
+                    }
                     int timeindex = rawevent.indexOf("pm,");
                     if ((timeindex == -1)||(timeindex>rawevent.indexOf("Read more"))){
                         timeindex = rawevent.indexOf("am,");
@@ -167,7 +228,7 @@ public class Spider2 {
                         if (rawevent.charAt(timeindex+4+j) == searchlocation.charAt(0+j)){
                             venue.append(rawevent.charAt(timeindex+4+j));
                         } else {
-                            venue.delete(venue.length()-1,venue.length());
+                           // venue.delete(venue.length()-1,venue.length());
                             break;
                         }
                     }
@@ -175,8 +236,8 @@ public class Spider2 {
                     rawevent = rawevent.substring(rawevent.indexOf("Read more") + 6);
                     body = body.substring(body.indexOf("Read more")+6);
                     event[5] = "School event";
-                    Event event1 = new Event(event[1],event[0],event[3],event[4],event[5],"1");
-                    week_events.add(event1);
+                   // Event event1 = new Event(event[2],event[0]+"|"+event[1],event[3],event[4],event[5],"1");
+                    week_events.add(event);
                 }
             }
         }
